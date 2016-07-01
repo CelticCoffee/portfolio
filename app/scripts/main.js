c75b02ba632dbde44fd5d121584fbaf374a5cbdd
Wendy <wendygoodwin@mac.com>
2016-07-01 11:40:35 -0700

    projects and services update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,4 +13,27 @@ $(document).ready(function() {
       $('.cathedralDescription').toggle();
       $('.leftCol').hide();
     });
+
+    $('.moreTextTrigger').click(function(){
+      $('.moreText').toggle('slow');
+    });
+
+    $('.cathedralTextShow').click(function(){
+      $('.moreCathedral').toggle('slow');
+      $('.cathedralSlide2').toggle('slow');
+      $('.cathedralTextShow').toggle('slow');
+      $('.backToCathedral').toggle('slow');
+    })
+
+    $('.backToCathedral').click(function(){
+      $('.moreCathedral').toggle('slow');
+      $('.cathedralSlide2').toggle('slow');
+      $('.cathedralTextShow').toggle('slow');
+      $('.backToCathedral').toggle('slow');
+    })
+
+    $('.topBtn').click(function(){
+      window.scrollTo(0, 14);
+      console.log('snowman');
+    })
 });
