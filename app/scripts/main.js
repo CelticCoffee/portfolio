65f023b14b5b8d0dfce45c40d07c85e5609a4f97
Wendy <wendygoodwin@mac.com>
2016-08-03 07:09:50 -0700

    found eslint issue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,17 +23,17 @@ $(document).ready(function() {
       $('.cathedralSlide2').toggle('slow');
       $('.cathedralTextShow').toggle('slow');
       $('.backToCathedral').toggle('slow');
-    })
+    });
 
     $('.backToCathedral').click(function(){
       $('.moreCathedral').toggle('slow');
       $('.cathedralSlide2').toggle('slow');
       $('.cathedralTextShow').toggle('slow');
       $('.backToCathedral').toggle('slow');
-    })
+    });
 
     $('.topBtn').click(function(){
       window.scrollTo(0, 14);
       console.log('snowman');
-    })
+    });
 });
